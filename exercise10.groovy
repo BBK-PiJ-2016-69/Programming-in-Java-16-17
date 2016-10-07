e1f4ff733d6beaf32850d1d31fd1fa0a625e700e
Matthew Grint <mattgrint@gmail.com>
2016-10-07 14:40:16 +0100

    Added first user input

--- exercise10.groovy
+++ exercise10.groovy
@@ -1,8 +1,30 @@
 int card1, card2, card3, card4, card5
-string suit1, suit2, suit3, suit4, suit5
+String suit1, suit2, suit3, suit4, suit5, input
 
 
 println "Please enter the cards in your hand:"
 println "Numbers 1-9, J, Q, K"
 println "Card 1 (rank):"
-card1 = System.console().readLine
+input = System.console().readLine()
+
+switch(input) {
+	case "J":
+		card1 = 10
+	break
+
+	case "Q":
+		card1 = 11
+	break
+
+	case "K":
+		card1 = 12
+	break
+
+	default: 
+		card1 = Integer.parseInt(input)
+	break
+}
+
+println "Card One:" + card1
+println "Suit:"
+suit = System.console().readLine()
