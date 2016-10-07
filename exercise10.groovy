7cb4a356279e0a56a3dcbd5c6111e2bd0531c6dc
Matthew Grint <mattgrint@gmail.com>
2016-10-07 14:24:16 +0100

    Adding new groovy file for calculating poker hand

---
+++ exercise10.groovy
@@ -0,0 +1,8 @@
+int card1, card2, card3, card4, card5
+string suit1, suit2, suit3, suit4, suit5
+
+
+println "Please enter the cards in your hand:"
+println "Numbers 1-9, J, Q, K"
+println "Card 1 (rank):"
+card1 = System.console().readLine
