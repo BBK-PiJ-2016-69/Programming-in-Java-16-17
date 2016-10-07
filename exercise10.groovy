0caa1b5ce5163f1fb1b1ed6a5b1ab44e990f3137
Matthew Grint <mattgrint@gmail.com>
2016-10-07 16:11:07 +0100

    Added logic for royal flush, flush and straight

--- exercise10.groovy
+++ exercise10.groovy
@@ -1,7 +1,12 @@
-def cards = [], suits = []
-int i = 1, j = 0, skip = 0
+def cards = [], suits = [], scards = []
+int i = 1, j = 0, skip = 0, max = 0, min = 15, h=0, d=0, c=0, s=0
+boolean samesuit = false, consecutive = false
 String input
 
+
+//
+// Capture the cards entered by the user
+//
 println "Please enter the cards in your hand:"
 println "Numbers 1-9, J, Q, K"
 println "Suits: H, C, S, D"
@@ -28,18 +33,26 @@ else
 switch(input) {
 	case "J":
 		cards << 10
+		if(10 > max) { max = 10 }
+		if(10 < min) { min = 10 }
 	break
 
 	case "Q":
 		cards << 11
+		if(11 > max) { max = 11 }
+		if(11 < min) { min = 11 }
 	break
 
 	case "K":
 		cards << 12
+		if(12 > max) { max = 12 }
+		if(12 < min) { min = 12 }
 	break
 
 	default: 
 		cards << Integer.parseInt(input)
+		if(Integer.parseInt(input)>max){max = Integer.parseInt(input)}
+		if(Integer.parseInt(input)<min){min = Integer.parseInt(input)}
 	break
 }
 }
@@ -59,19 +72,56 @@ else
 {
 	skip = 0;
 	suits << input
-	i++;
-
+	i++
 }
 
 }
 
-println "Card 1: " + cards[0]
-println "Suits1: " + suits[0]
-println "Card2: " + cards[1]
-println "Suits2: " + suits[1]
-println "Card3: " + cards[2]
-println "Suits3: " + suits[2]
-println "Card4: " + cards[3]
-println "Suits4: " + suits[3]
-println "Card5: " + cards[4]
-println "Suits5: " + suits[4]
+//
+//  Check the value of the cards
+//
+
+if (max - min == 4) { consecutive = true }
+
+
+p = 0
+while (p < 4)
+{
+if (suits[p] == "C"){ c++ }
+if (suits[p] == "S"){ s++ }
+if (suits[p] == "H"){ h++ }
+if (suits[p] == "D"){ d++ }
+p++
+}
+
+if(c == 4 || s == 4 || h == 4 || d == 4)
+{
+	samesuit = true
+}
+
+
+
+// Is it a royal flush?
+
+if (consecutive == true && samesuit == true)
+{
+	println "Royal Flush"
+}
+
+
+// Is it a flush?
+
+if (consecutive == false && samesuit == true)
+{
+	println "Flush"
+}
+
+// Is it a straight?
+
+if (consecutive == true && samesuit == false)
+{
+	println "Straight"
+}
+
+
+
