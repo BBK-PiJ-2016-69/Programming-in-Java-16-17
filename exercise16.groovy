d4ec1cf5d9f4aecc42218583ffe1d459119c6fd3
Matthew Grint <mattgrint@gmail.com>
2016-10-08 15:21:03 +0100

    First commit of Exercise 16

---
+++ exercise16.groovy
@@ -0,0 +1,13 @@
+// Define Variables
+String playerOne, playerTwo, playersHand
+int playerOneWins, playerTwoWins, lead
+
+
+// Enter intial loop
+while (playerOneWins - playerTwoWins !< -3 && playerOneWins - playerTwoWins !> 3)
+{
+	// Gather input
+	println "Please enter the match result in the format R = Rock, P = Paper, S = Stone (e.g. RR)"
+
+	playerOne = playersHand.substring(0)
+}
