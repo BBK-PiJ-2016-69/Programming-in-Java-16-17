a55a92abdabe9399424dc374c7b9b27e8426d9f2
Matthew Grint <mattgrint@gmail.com>
2016-10-08 15:40:11 +0100

    First draft

---
+++ exercise17.groovy
@@ -0,0 +1,9 @@
+// Declare variables
+int i, j, givenNumber
+String input
+
+// Read input from user
+println "Please enter your integer"
+input = System.console().readLine()
+givenNumber = Integer.parseInt(input)
+
