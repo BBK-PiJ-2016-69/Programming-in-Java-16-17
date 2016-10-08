662c1cda3328d8a356a2553ce2ece683f01f8ad7
Matthew Grint <mattgrint@gmail.com>
2016-10-08 15:55:13 +0100

    Finalised calculation

--- exercise17.groovy
+++ exercise17.groovy
@@ -1,9 +1,67 @@
 // Declare variables
-int i, j, givenNumber
+int i, j = 2, givenNumber, lowerPrime, higherPrime
 String input
+boolean prime
 
 // Read input from user
 println "Please enter your integer"
 input = System.console().readLine()
 givenNumber = Integer.parseInt(input)
 
+
+while (i <= givenNumber){
+ prime = true
+
+	while (j < i){
+
+ 		if (i % j == 0){
+ 		prime = false
+ 		}
+
+ 		j++
+
+
+ 	}
+
+ 	if(prime == true){ lowerPrime = i}
+
+ 	j=2
+ 	i++;
+
+}
+
+
+i = givenNumber
+
+while (i <= givenNumber + (givenNumber - lowerPrime)){
+ prime = true
+
+	while (j < i){
+
+ 		if (i % j == 0){
+ 		prime = false
+ 		}
+
+ 		j++
+
+
+ 	}
+
+ 	if(prime == true){ higherPrime = i}
+
+ 	j=2
+ 	i++;
+
+}
+
+
+if(higherPrime == 0){
+	println "Lower Prime Closest:" + lowerPrime
+}
+else if (higherPrime - givenNumber < givenNumber - lowerPrime){
+	println "Higher Prime Closest: " + higherPrime	
+}
+else if (higherPrime - givenNumber == givenNumber - lowerPrime){
+	println "Both Primes equally close, higher is: " + higherPrime	
+	println "Lower is:" + lowerPrime
+}
