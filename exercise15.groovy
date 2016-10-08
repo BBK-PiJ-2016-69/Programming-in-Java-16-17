51401bcdb3e6ce5292e8f8bae5a9cc09e4311034
Matthew Grint <mattgrint@gmail.com>
2016-10-08 15:15:37 +0100

    Finalised Exercise 15

--- exercise15.groovy
+++ exercise15.groovy
@@ -1,12 +1,41 @@
 // Define variables
 String input
-int guess, randomNumber
+int guess, randomNumber, numberOfGuesses
+boolean correct = false
+
+// Generate random number
+randomNumber = Math.abs(1000 * Math.random())
 
 // Initial output
 println "Try to guess my number!"
-println "Tell me a number: "
 
-// Gather input and convert
-input = System.console().readLine()
-guess = Integer.parseInt(input)
+
+while (correct != true){
+
+	// Increase number of guesses
+	numberOfGuesses++
+
+	// Gather input and convert to integer
+	println "Tell me a number: "
+	input = System.console().readLine()
+	guess = Integer.parseInt(input)
+
+	// Check if guess is true
+	if (guess == randomNumber){
+		println "Correct!"
+		println "You needed " + numberOfGuesses + " guesses"
+		correct == true
+		break
+	}
+
+	else if (guess > randomNumber){
+		println "No! My number is lower"
+	}
+
+	else if (guess < randomNumber){
+		println "No! My number is higher"
+	}
+
+
+}
 
