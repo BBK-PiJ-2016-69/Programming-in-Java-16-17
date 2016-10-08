15f84794bb5b76f03c6ee71e9946349d06321a3e
Matthew Grint <mattgrint@gmail.com>
2016-10-08 16:39:09 +0100

    Finalised exercise 18

--- exercise18.groovy
+++ exercise18.groovy
@@ -1,5 +1,6 @@
 // Define variables
-int denominator, givenNumber, i, answer = 0
+int denominator = 1, givenNumber, i=1
+double answer
 boolean add = true
 String input
 
@@ -8,7 +9,34 @@ println "Please enter the number of terms to calculate: "
 input = System.console().readLine()
 givenNumber = Integer.parseInt(input)
 
-while (i <= givenNumber){
+// Loop while within chosen number of terms
+while (i < givenNumber){
 
+	// Deal within intial case
+	if(i == 1){
+		answer = 4/denominator
+	}
 
+	// Deal with addition term
+	else if (add == true){
+		answer = answer + (4/denominator)
+	}
+
+	// Deal with subtration term
+	else if (add == false){
+		answer = answer - (4/denominator)
+	}
+
+	// Update denominator
+	denominator = (2 * i) + 1
+
+	// Update loop
+	i++
+
+	// Invert term
+	add = ! add
 }
+
+// Display output
+println "Answer: " + answer
+println "For given number of terms: " + givenNumber
