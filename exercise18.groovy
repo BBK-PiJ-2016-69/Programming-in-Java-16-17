d25099eda6e880789c90df61217c8a271510c65c
Matthew Grint <mattgrint@gmail.com>
2016-10-08 16:26:35 +0100

    Initial commit of exercise 18

---
+++ exercise18.groovy
@@ -0,0 +1,14 @@
+// Define variables
+int denominator, givenNumber, i, answer = 0
+boolean add = true
+String input
+
+// Get user input
+println "Please enter the number of terms to calculate: "
+input = System.console().readLine()
+givenNumber = Integer.parseInt(input)
+
+while (i <= givenNumber){
+
+
+}
