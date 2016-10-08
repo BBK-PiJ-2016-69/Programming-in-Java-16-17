ea6a6c69d8ee306ea3ca6b7a2d8c85344e4bf105
Matthew Grint <mattgrint@gmail.com>
2016-10-08 14:59:59 +0100

    Finished exercise 13

--- exercise13.groovy
+++ exercise13.groovy
@@ -1,19 +1,24 @@
-int i, j
+int i, j = 2
 boolean prime
 
 while (i <= 1000){
  prime = true
+
 	while (j < i){
- 		if (j % i == 0 && j! = 1){
+		//println "j % i = ?"
+		// println j + "%" + i + " = " + j % i
+ 		if (i % j == 0){
  		prime = false
  		}
+
  		j++
 
- 	}
- 	if(prime == true){
- 		println i
+
  	}
 
+ 	if(prime == true){ println i}
+
+ 	j=2
  	i++;
 
 }
