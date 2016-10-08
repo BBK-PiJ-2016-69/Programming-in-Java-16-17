a14589187cc00c1b1408ebaa94970690f9de193e
Matthew Grint <mattgrint@gmail.com>
2016-10-08 15:01:41 +0100

    First commit of exercise 14

---
+++ exercise14.groovy
@@ -0,0 +1,25 @@
+int i, j = 2, primeCount = 0
+boolean prime
+
+while (i <= 1000){
+ prime = true
+
+	while (j < i){
+		//println "j % i = ?"
+		// println j + "%" + i + " = " + j % i
+ 		if (i % j == 0){
+ 		prime = false
+ 		}
+
+ 		j++
+
+
+ 	}
+
+ 	if(prime == true){ println i}
+
+ 	j=2
+ 	i++;
+
+}
+	
