72b155d62e5732dee414f96aa13c9f3adb757ec8
Matthew Grint <mattgrint@gmail.com>
2016-10-08 15:31:41 +0100

    Fixed bug with variable name

--- exercise16.groovy
+++ exercise16.groovy
@@ -1,13 +1,82 @@
 // Define Variables
 String playerOne, playerTwo, playersHand
-int playerOneWins, playerTwoWins, lead
+int playerOneWins, playerTwoWins
 
 
 // Enter intial loop
-while (playerOneWins - playerTwoWins !< -3 && playerOneWins - playerTwoWins !> 3)
+while ((playerOneWins - playerTwoWins) > -4 && (playerOneWins - playerTwoWins) < 4)
 {
 	// Gather input
 	println "Please enter the match result in the format R = Rock, P = Paper, S = Stone (e.g. RR)"
+	playersHand = System.console().readLine()
+	playerOne = playersHand.substring(0,1)
+	playerTwo = playersHand.substring(1)
 
-	playerOne = playersHand.substring(0)
+	// Calculate if draw
+	if (playerOne == playerTwo){
+		println "Draw"
+	}
+
+	// Calculate if playerOne is rock
+	if (playerOne == "R"){
+		if(playerTwo == "P")
+		{
+			println "Player Two Wins"
+			playerTwoWins++
+		}
+
+		if(playerTwo == "S")
+		{
+			println "Player One Wins"
+			playerOneWins++
+		}
+
+	}
+
+	// Calculate if playerOne is paper
+	if (playerOne == "P"){
+		if(playerTwo == "R")
+		{
+			println "Player One Wins"
+			playerOneWins++
+		}
+
+		if(playerTwo == "S")
+		{
+			println "Player Two Wins"
+			playerTwoWins++
+		}
+
+	}
+
+	// Calculate if playerOne is scissors
+	if (playerOne == "S"){
+		if(playerTwo == "R")
+		{
+			println "Player Two Wins"
+			playerTwoWins++
+		}
+
+		if(playerTwo == "P")
+		{
+			println "Player One Wins"
+			playerOneWins++
+		}
+
+	}
+
 }
+
+// Declare winner
+
+if (playerOneWins > playerTwoWins){
+	println "Player One Wins"
+}
+else
+{
+	println "Player Two Wins"
+}
+
+
+
+
