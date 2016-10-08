c07c5a842e1d7f6e1f81f1611c5b0f2b9c5f2777
Matthew Grint <mattgrint@gmail.com>
2016-10-08 15:06:30 +0100

    Initial commit of exercise 15

---
+++ exercise15.groovy
@@ -0,0 +1,12 @@
+// Define variables
+String input
+int guess, randomNumber
+
+// Initial output
+println "Try to guess my number!"
+println "Tell me a number: "
+
+// Gather input and convert
+input = System.console().readLine()
+guess = Integer.parseInt(input)
+
