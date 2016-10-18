bebdf09674580e32915d0679c44ff06e0c532019
Matthew Grint <mattgrint@gmail.com>
2016-10-18 12:08:46 +0100

    Completed exercise

--- Day3Exercise13.groovy
+++ Day3Exercise13.groovy
@@ -1,10 +1,37 @@
 String userInput
+int loopTimes = 0, i = 0, stringLength
+boolean isPalindrome = false
 
 println "Please enter your palindrome: "
 userInput = System.console().readLine()
 
-if(userInput.length() % 2 != 0){
-	
+stringLength = userInput.length()
+
+loopTimes = stringLength
+
+if(loopTimes % 2 != 0){
+	loopTimes = loopTimes - 1
 }
 
-for(i = 0, i)
+loopTimes = loopTimes / 2
+
+while (i < loopTimes){
+	if(userInput.charAt(i) == userInput.charAt(stringLength-i-1))
+	{
+		palindrome = true
+	}
+	else
+	{
+		palindrome =  false
+	}
+
+	i++;
+}
+
+if(palindrome == true){
+	println "This is a palindrome."
+}
+else
+{
+	println "This is not a palindrome."
+}
