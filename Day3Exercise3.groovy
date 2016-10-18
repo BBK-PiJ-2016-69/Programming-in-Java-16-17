37ce6ef165b9107d45d0e6765292fa5e49280214
Matthew Grint <mattgrint@gmail.com>
2016-10-18 09:09:50 +0100

    Finalised calculation

--- Day3Exercise3.groovy
+++ Day3Exercise3.groovy
@@ -1,5 +1,5 @@
-double termOne, termTwo, result
-String userInput, operator
+Double termOne, termTwo, result
+String userInput, operator, termOneString, termTwoString
 
 println "Please enter a mathematical operation (e.g. 4*6): "
 userInput  = System.console().readLine()
@@ -7,20 +7,45 @@ userInput  = System.console().readLine()
 userInput = userInput.replaceAll(" ","")
 
 if(userInput.indexOf("+") != -1){
-	println userInput.indexOf("+")
+
+	termOneString = userInput.substring(0,userInput.indexOf("+"))
+	termOne = Double.parseDouble(termOneString)
+
+	termTwoString = userInput.substring(userInput.indexOf("+")+1,userInput.length())
+	termTwo = Double.parseDouble(termTwoString)
+	
+	result = termOne + termTwo
 }
 
 if(userInput.indexOf("-") != -1){
-	println userInput.indexOf("-")
+	termOneString = userInput.substring(0,userInput.indexOf("-"))
+	termOne = Double.parseDouble(termOneString)
+
+	termTwoString = userInput.substring(userInput.indexOf("-")+1,userInput.length())
+	termTwo = Double.parseDouble(termTwoString)
+	
+	result = termOne - termTwo
 }
 
 if(userInput.indexOf("/") != -1){
-	println userInput.indexOf("/")
+	termOneString = userInput.substring(0,userInput.indexOf("/"))
+	termOne = Double.parseDouble(termOneString)
+
+	termTwoString = userInput.substring(userInput.indexOf("/")+1,userInput.length())
+	termTwo = Double.parseDouble(termTwoString)
+	
+	result = termOne / termTwo
 }
 
 if(userInput.indexOf("*") != -1){
-	println userInput.indexOf("*")
+	termOneString = userInput.substring(0,userInput.indexOf("*"))
+	termOne = Double.parseDouble(termOneString)
+
+	termTwoString = userInput.substring(userInput.indexOf("*")+1,userInput.length())
+	termTwo = Double.parseDouble(termTwoString)
+	
+	result = termOne * termTwo
 }
 
-
+println "Result: " + result
 
