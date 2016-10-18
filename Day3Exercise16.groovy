0ae93b484e826a0ed291d60793f2c1e88d6884dd
Matthew Grint <mattgrint@gmail.com>
2016-10-18 12:43:11 +0100

    Completed exercise

--- Day3Exercise16.groovy
+++ Day3Exercise16.groovy
@@ -1,15 +1,25 @@
 String userInput, startOfNumber, endOfNumber
 boolean decimal=false
+int beforeDecimal, afterDecimal
 
 
 println "Please enter your number to halve: "
 userInput = System.console().readLine()
 
 startOfNumber = userInput.substring(0,userInput.indexOf("."))
-endOfNumber = userInput.substring(userInput.indexOf("."),userInput.length())
+endOfNumber = userInput.substring(userInput.indexOf(".")+1,userInput.length())
+
 
 println "Start: " + startOfNumber
 println "End: " + endOfNumber
 
+
+beforeDecimal = Integer.parseInt(startOfNumber)
+afterDecimal = Integer.parseInt(endOfNumber)
+
+beforeDecimal = beforeDecimal / 2
+afterDecimal = afterDecimal / 2
+
+println "Half of " + userInput + " is " + beforeDecimal + "." + afterDecimal
 
 
