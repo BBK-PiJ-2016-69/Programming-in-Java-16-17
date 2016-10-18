9660cb01af91d00ea722f160be3e804797025777
Matthew Grint <mattgrint@gmail.com>
2016-10-18 10:50:46 +0100

    Completed first part of task

--- Day3Exercise9.groovy
+++ Day3Exercise9.groovy
@@ -1,2 +1,16 @@
-String userInput
-int i 
+String userInput, toPrint = ""
+int letterCount = 0
+
+
+// Get user input
+println "Please enter your text: "
+userInput = System.console().readLine()
+
+for (i = 0; i < userInput.length(); i++) {
+
+   if(userInput.charAt(i) == "e"){
+   		letterCount++ 
+   }
+}
+
+println letterCount
