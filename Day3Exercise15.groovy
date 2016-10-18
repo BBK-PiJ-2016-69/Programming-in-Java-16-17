4a08afc205ecf807506923501fc119bde49ffdfa
Matthew Grint <mattgrint@gmail.com>
2016-10-18 12:35:38 +0100

    Finalised task

--- Day3Exercise15.groovy
+++ Day3Exercise15.groovy
@@ -1,5 +1,6 @@
 String userInput, cleanInput = ""
-int i 
+int i, stringLength, loopTimes
+boolean palindrome
 
 println "Please enter your palindrome: "
 userInput = System.console().readLine()
@@ -13,4 +14,36 @@ for (i = 0; i < userInput.length() ; i++){
 
 }
 
-println cleanInput
+
+stringLength = cleanInput.length()
+loopTimes = stringLength
+
+if(loopTimes % 2 != 0){
+	loopTimes = loopTimes - 1
+}
+
+loopTimes = loopTimes / 2
+
+i = 0;
+
+while (i < loopTimes){
+
+	if(cleanInput.charAt(i) == cleanInput.charAt(stringLength-i-1))
+	{
+		palindrome = true
+	}
+	else
+	{
+		palindrome =  false
+	}
+
+	i++;
+}
+
+if(palindrome == true){
+	println "This is a palindrome."
+}
+else
+{
+	println "This is not a palindrome."
+}
