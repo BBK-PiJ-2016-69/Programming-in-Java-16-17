51012aaf33aa40da7bf008d65298bd66f0eddd4e
Matthew Grint <mattgrint@gmail.com>
2016-10-18 12:20:24 +0100

    Completed task

--- Day3Exercise14.groovy
+++ Day3Exercise14.groovy
@@ -1,1 +1,16 @@
-string userInput
+String userInput, secondHalf = ""
+
+
+println "Please enter your text to make into a palindrome: "
+userInput = System.console().readLine()
+
+
+for(i=0 ; i < userInput.length() ; i++){
+
+	 secondHalf = secondHalf + userInput.charAt(userInput.length()-i-1)
+}
+
+println userInput + secondHalf
+
+
+
