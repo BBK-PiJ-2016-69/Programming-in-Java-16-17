5313cf69f1d2944375828de979503f8648e594ea
Matthew Grint <mattgrint@gmail.com>
2016-10-18 11:58:39 +0100

    Finalised exercise

--- Day3Exercise12.groovy
+++ Day3Exercise12.groovy
@@ -1,8 +1,8 @@
-double purchasePrice, cashGiven, change, displayCurrency
+float purchasePrice, cashGiven, change
 def currencyAvailable = [50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01]
-def displayCurrency = ["£50s","£20s","£10s"]
+def displayCurrency = ["£50s","£20s","£10s","£5s","£2s","£1s","50ps","20ps","10ps","5ps","2ps","1ps"]
 String userInput
-int i 
+int i = 0 
 
 // Get user input
 println "Please enter the price of the purchase:"
@@ -20,15 +20,10 @@ change = cashGiven - purchasePrice
 while (i < currencyAvailable.size()){
 
 	if(change > currencyAvailable[i]){
-		if(currencyAvailable[i]>=1){
-			println "£" + currencyAvailable[i] +"s: " + (change/currencyAvailable[i]).trunc()
-		}
-		else
-		{
-			displayCurrency = (currencyAvailable[i]*100)
-			println "" + displayCurrency.trunc() + "ps: " + (change/currencyAvailable[i]).trunc()
-		}
+
+		println displayCurrency[i] + ": " + (change/currencyAvailable[i]).trunc()
 		change = change % currencyAvailable[i]
+
 	}
 
 	i++
