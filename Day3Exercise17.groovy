02d811a95a4d474f9dff3246851dd40366dc2d2e
Matthew Grint <mattgrint@gmail.com>
2016-10-18 13:42:40 +0100

    Building DATA input

--- Day3Exercise17.groovy
+++ Day3Exercise17.groovy
@@ -1,4 +1,5 @@
 String userInput, fromUser, fromDomain, toUser, toDomain
+def wholeEmail = []
 boolean validCommand = false, quit = false
 
 println "Welcome to my mailserver"
@@ -50,4 +51,76 @@ while (validCommand == false){
 
 }
 
-$validCommand = false
+validCommand = false
+
+// Get the recipient from command or quit
+while (validCommand == false){
+	print ">>> "
+	userInput = System.console().readLine()
+
+	if(userInput.substring(0,4) == "QUIT"){
+		quit = true
+		println "QUIT"
+		validCommand = true
+ 	}
+
+ 	if(userInput.length() > 8){
+
+		if(userInput.substring(0,8) == "RCPT TO:"){
+
+			if(userInput.indexOf("@")!=-1){
+				
+				toUser = userInput.substring(userInput.indexOf(":")+1,userInput.indexOf("@"))
+				toDomain = userInput.substring(userInput.indexOf("@")+1,userInput.length())
+
+				if(toUser.trim().length()>0 && fromDomain.trim().length()>0){
+					println "OK"
+					validCommand = true
+				}
+				else
+				{
+					println "Invalid user or domain"
+				}
+			}
+			else
+			{
+				println "Invalid Command"
+			}
+ 		}
+ 		else
+ 		{
+ 			println "Invalid Command"
+ 		}
+	}
+	else
+	{
+		println "Invalid Command"
+	}
+
+}
+
+
+// Get the data
+while (validCommand == false){
+	print ">>> "
+	userInput = System.console().readLine()
+
+	if(userInput.substring(0,4) == "QUIT"){
+		quit = true
+		println "QUIT"
+		validCommand = true
+ 	}
+ 	else if(userInput.substring(0,4) == "DATA") {
+ 		while(emailLine != "."){
+ 			emailLine = System.console().readLine()
+ 			wholeEmail << emailLine
+ 		}
+ 		validCommand = true
+ 	}
+ 	else
+ 	{
+ 		println "Invalid Command"
+ 	}
+}
+
+
