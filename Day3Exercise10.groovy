d4911e34588068f1f98e1c7e210008980cf9c7ce
Matthew Grint <mattgrint@gmail.com>
2016-10-18 11:03:48 +0100

    Completed the task

--- Day3Exercise10.groovy
+++ Day3Exercise10.groovy
@@ -1,5 +1,6 @@
 String userInput,searchString
-int offset = 0
+int offset = 0, matchCount = 0
+boolean match = false
 
 
 // Get user input
@@ -9,7 +10,24 @@ userInput = System.console().readLine()
 println "Please enter the string for which to search: "
 searchString = System.console().readLine()
 
+while(offset < userInput.length()-searchString.length())
+{
+	if (match == true){
+		matchCount++
+	}
 
-for (i = 0; i < searchString.length(); i++) {
-	println userInput.charAt(i+offset)
+	match = false
+
+	for (i = 0; i < searchString.length(); i++) {
+		if(userInput.charAt(i+offset) == searchString.charAt(i)){
+			match = true
+		}
+		else
+		{
+			match = false
+		}
+	}
+	offset++;
 }
+
+println "Matches: " + matchCount
