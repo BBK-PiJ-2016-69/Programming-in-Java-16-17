ef5d53092bd55bdf507b48de7a1388bed49be0ad
Matthew Grint <mattgrint@gmail.com>
2016-10-19 10:58:44 +0100

    Added additional power function and started adding binary2decimal conversion

--- Day4Exercise3.groovy
+++ Day4Exercise3.groovy
@@ -2,6 +2,7 @@ String userInput
 int base, exponent, result
 boolean running = true
 
+
 int power(b, e){
 	int originalBase = b
 
@@ -12,24 +13,47 @@ int power(b, e){
 	return b
 }
 
+int binary2decimal(binaryString){
+
+	for (i = 0 ; i <= binaryString.length() ; i++){
+		
+	}
+}
+
 
 void getPower(){
-println "Please enter the base: "
-userInput = System.console().readLine()
-base = Integer.parseInt(userInput)
+	println "Please enter the base: "
+	userInput = System.console().readLine()
+	base = Integer.parseInt(userInput)
 
-println "Please enter the exponent: "
-userInput = System.console().readLine()
-exponent = Integer.parseInt(userInput)
+	println "Please enter the exponent: "
+	userInput = System.console().readLine()
+	exponent = Integer.parseInt(userInput)
 
-result = power(base,exponent)
+	result = power(base,exponent)
 
-println result
+	println result
+}
+
+void getPower2(){
+	println "Please enter the exponent: "
+	userInput = System.console().readLine()
+	exponent = Integer.parseInt(userInput)
+
+	println power(2,exponent)
+}
+
+void getBinary2Decimal(){
+	println "Please enter a binary number"
+	userInput = System.console().readLine()
+
+	println binary2decimal(userInput)
 }
 
 
 while(running == true){
-
+	println ""
+	println "------------------------------------------"
 	println "Please enter your selection:"
 	println "1) Power of two numbers"
 	println "2) Power of 2 to chosen exponent"
@@ -45,7 +69,26 @@ while(running == true){
 		case "0":
 			running = false
 			break;
-	}
+
+	case "1":
+			getPower()
+			break;
+
+	case "2":
+			getPower2()
+			break;
+
+	case "3":
+			getBinary2Decimal()
+			break;
+
+	default:
+			println "Error: Lazy programmer has not yet coded this bit"
+			break;
+
+
+	}		
+
 }
 
 println "Thanks for using this program"
