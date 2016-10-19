f67c856c2e577b079a4d463cf1be6e7ca6624c25
Matthew Grint <mattgrint@gmail.com>
2016-10-19 15:35:38 +0100

    Added easily improvable solution for hex2binary

--- Day4Exercise4.groovy
+++ Day4Exercise4.groovy
@@ -1,8 +1,66 @@
 String userInput
 
-
 String hex2binary(hex){
-	return hex
+
+
+	String binary = ""
+
+	for(i = 0; i < hex.length() ; i++){
+
+		if (hex.charAt(i) == "0"){
+			binary = binary + " 0000"
+		}
+		else if (hex.charAt(i) == "1"){
+			binary = binary + " 0001"
+		}
+		else if (hex.charAt(i) == "2"){
+			binary = binary + " 0010"
+		}
+		else if (hex.charAt(i) == "3"){
+			binary = binary + " 0011"
+		}
+		else if (hex.charAt(i) == "4"){
+			binary = binary + " 0100"
+		}
+		else if (hex.charAt(i) == "5"){
+			binary = binary + " 0101"
+		}
+		else if (hex.charAt(i) == "6"){
+			binary = binary + " 0110"
+		}
+		else if (hex.charAt(i) == "7"){
+			binary = binary + " 0111"
+		}
+		else if (hex.charAt(i) == "8"){
+			binary = binary + " 1000"
+		}
+		else if (hex.charAt(i) == "9"){
+			binary = binary + " 1001"
+		}
+		else if (hex.charAt(i) == "A"){
+			binary = binary + " 1010"
+		}
+		else if (hex.charAt(i) == "B"){
+			binary = binary + " 1011"
+		}
+		else if (hex.charAt(i) == "C"){
+			binary = binary + " 1100"
+		}
+		else if (hex.charAt(i) == "D"){
+			binary = binary + " 1101"
+		}
+		else if (hex.charAt(i) == "E"){
+			binary = binary + " 1110"
+		}
+		else if (hex.charAt(i) == "F"){
+			binary = binary + " 1111"
+		}
+		else {
+			println "Error"
+		}
+	}
+
+	return binary
 }
 
 String binary2hex(binaryString){
@@ -100,7 +158,7 @@ userInput = System.console().readLine()
 if(userInput.length() > 2){
 
 	if(userInput.substring(0,2)=="0x"){
-		println hex2binary(userInput.substring(2,userInput.length()))
+		println hex2binary(userInput.substring(2,userInput.length()).toUpperCase())
 	}
 	else
 	{
