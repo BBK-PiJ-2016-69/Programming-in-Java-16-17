b25675f01f72d35d7a5068a49d691c8d79448296
Matthew Grint <mattgrint@gmail.com>
2016-10-20 14:29:13 +0100

    Completed task with very simple interest calculation

--- Day4Exercise7.groovy
+++ Day4Exercise7.groovy
@@ -1,6 +1,6 @@
 double totalPaid(double totalBorrowed, double rate, double years){
 	
-	double totalPaid = totalBorrwed * (1 + (rate/100))
+	double totalPaid = totalBorrowed * (1 + (rate/100))
 
 	return totalPaid 
 }
@@ -10,10 +10,11 @@ double yearlyPaid(double toPay, double years){
 	return toPay / years
 }
 
-double interestPaid(double totalBorrowed, double rate, double years){
+double timeUntilInterestPaid(double totalBorrowed, double totalPaid, double yearlyPaid){
 
-	return 0
-	
+	timeUntilInterestPaid = (totalPaid-totalBorrowed)/yearlyPaid
+
+	return timeUntilInterestPaid
 }
 
 
@@ -24,26 +25,32 @@ double totalBorrowed, years, rate
 println "Please enter the total amount to be borrowed: "
 print ">>"
 userInput = System.console().readLine()
-totalBorrwed = Double.parseDouble(userInput)
+totalBorrowed = Double.parseDouble(userInput)
 
 println "Please enter the years over which the mortgage will be repaid: "
 print ">>"
 userInput = System.console().readLine()
 years = Double.parseDouble(userInput)
 
-println "Please enter the rate (assume fixed over life of mortgage): "
+println "Please enter the rate (assume simple, non compounding interest): "
 print ">>"
 userInput = System.console().readLine()
 rate = Double.parseDouble(userInput)
 
 
 // Total amount to be paid test
-println "Total Paid: " + Math.round(totalPaid(totalBorrowed, rate, years))
+totalPaid = totalPaid(totalBorrowed, rate, years)
+println "Total Paid: " + Math.round(totalPaid)
 
 // Money to be paid every year test
-println "Paid each year: " + Math.round(yearlyPaid(totalPaid(totalBorrowed,rate,years), years))
+yearlyPaid = yearlyPaid(totalPaid, years)
+println "Paid each year: " + Math.round(yearlyPaid)
 
 // Time before interest is repaid
+timeUntilInterestPaid = timeUntilInterestPaid(totalBorrowed, totalPaid, yearlyPaid)
+println "Time until interest paid: " + Math.round(timeUntilInterestPaid)
+
+
 
 
 
