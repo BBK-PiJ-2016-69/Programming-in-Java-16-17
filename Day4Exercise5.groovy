62a30e4b86bedff4bea0d985ea7ae66172185b5c
Matthew Grint <mattgrint@gmail.com>
2016-10-20 13:25:59 +0100

    Added distanceToOrigin function

--- Day4Exercise5.groovy
+++ Day4Exercise5.groovy
@@ -5,19 +5,34 @@ class Point {
 	double distanceTo(Point passedPoint){
 		double distance = ((this.x - passedPoint.x)**2) + ((this.y - passedPoint.y)**2)
 		distance = Math.sqrt(distance)
+
+		return distance
 	}	
+
+	double distanceToOrigin(){
+		Point origin = new Point()
+		origin.x = 0
+		origin.y = 0
+
+		return distanceTo(origin)
+	}
 }
 
 
 
-
+// DistanceTo
 Point myPointOne = new Point()
 myPointOne.x = 10
 myPointOne.y = 12
 
 Point myPointTwo = new Point()
-myPointOne.x = 12
-myPointOne.y = 14
+myPointTwo.x = 12
+myPointTwo.y = 14
 
 println "The distance between points is " + Math.abs(myPointOne.distanceTo(myPointTwo))
 println "This is equivalent to: " + Math.abs(myPointTwo.distanceTo(myPointOne))
+
+
+// DistanceToOrigin
+println "The distance from myPointOne to origin is " + myPointOne.distanceToOrigin()
+println "The distance from myPointTwo to origin is " + myPointTwo.distanceToOrigin()
