65c0b1559e5f654b890b77e360b54a0d187835fa
Matthew Grint <mattgrint@gmail.com>
2016-10-20 13:46:43 +0100

    Tested and completed exercise

--- Day4Exercise6.groovy
+++ Day4Exercise6.groovy
@@ -9,7 +9,7 @@ class Integer2 {
 		this.value = setTo
 	}
 
-	boolean isTrue(){
+	boolean isEven(){
 		if (this.value % 2 == 0){
 			return true
 		}
@@ -38,5 +38,21 @@ class Integer2 {
 }
 
 
-
+Integer2 i2 = new Integer2();
+print "Enter a number: ";
+String str = System.console().readLine();
+int i = Integer.parseInt(str);
+i2.setValue(i);
+print "The number you entered is "
+if (i2.isEven()) {
+println "even.";
+} else if (i2.isOdd()) {
+println "odd.";
+} else {
+println "undefined!! Your code is buggy!";
+}
+int parsedInt = Integer.parseInt(i2.toString());
+if (parsedInt == i2.getValue()) {
+println("Your toString() method seems to work fine.");
+}
 
