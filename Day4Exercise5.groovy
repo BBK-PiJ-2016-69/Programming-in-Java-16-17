737f82c555e24ecaa23dd8d057a187e8a4b50fac
Matthew Grint <mattgrint@gmail.com>
2016-10-20 10:56:15 +0100

    Completed distanceTo() method and tested

--- Day4Exercise5.groovy
+++ Day4Exercise5.groovy
@@ -1,11 +1,23 @@
 class Point {
 	double x
 	double y 
+
+	double distanceTo(Point passedPoint){
+		double distance = ((this.x - passedPoint.x)**2) + ((this.y - passedPoint.y)**2)
+		distance = Math.sqrt(distance)
+	}	
 }
 
 
-Point myPoint = new Point()
-myPoint.x = 10
-myPoint.y = 12
 
-println "(x,y) = (" + myPoint.x + "," + myPoint.y + ")"
+
+Point myPointOne = new Point()
+myPointOne.x = 10
+myPointOne.y = 12
+
+Point myPointTwo = new Point()
+myPointOne.x = 12
+myPointOne.y = 14
+
+println "The distance between points is " + Math.abs(myPointOne.distanceTo(myPointTwo))
+println "This is equivalent to: " + Math.abs(myPointTwo.distanceTo(myPointOne))
