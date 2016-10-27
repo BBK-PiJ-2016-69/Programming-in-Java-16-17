cf83313542ee5ea56db6f6dee3bdd5e8a8a66b73
Matthew Grint <mattgrint@gmail.com>
2016-10-27 14:01:52 +0100

    Error checking underway

--- EmployeeDatabase.java
+++ EmployeeDatabase.java
@@ -11,10 +11,11 @@ public class EmployeeDatabase {
 
 		String userInput, employeeName;
 		boolean completed = false;
-		int employeeNumber;
+		int employeeNumber, i;
+		EmployeeDatabase databaseOne = new EmployeeDatabase();
 
 		while(completed == false){
-			EmployeeDatabase databaseOne = new EmployeeDatabase();
+			
 			System.out.println("Please enter employee name: ");
 			System.out.print(">>");
 			userInput = System.console().readLine();
@@ -41,6 +42,11 @@ public class EmployeeDatabase {
 
 		}
 
+		for(i=0; i<databaseOne.employeeNames.length; i++){
+			System.out.println(databaseOne.employeeNames[i]);
+		}
+		
+
 	}
 
 	public void addEmployee(String name, int number){
@@ -48,7 +54,7 @@ public class EmployeeDatabase {
 
 
 		if(this.employeeCount > this.tempNames.length){
-			 this.tempNames = this.expand(tempNames);
+			this.tempNames = this.expand(tempNames);
 			this.tempNumbers = this.expand(tempNumbers);
 
 		}
@@ -56,12 +62,24 @@ public class EmployeeDatabase {
 	}
 
 	public String[] expand(String[] tempNames){
+		int i;
 		String[] newNames = new String[tempNames.length*2];
+
+
+		for(i = 0; i < tempNames.length; i++){
+			newNames[i] = tempNames[i];
+		}
 		return newNames;
 	}
 
 	public int[] expand(int[] tempNumbers){
+		int i;
 		int[] newNumbers = new int[tempNumbers.length*2];
+
+		for(i = 0; i < tempNumbers.length; i++){
+			newNumbers[i] = tempNumbers[i];
+		}
+
 		return newNumbers;
 	}
 
