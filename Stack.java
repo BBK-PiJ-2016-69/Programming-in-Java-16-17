ffa00afb56102bde2c086dfd7088422c99ecd3e8
Matthew Grint <mattgrint@gmail.com>
2016-10-31 13:39:00 +0000

    Completed program with push/pop functionality and empty checker

--- Stack.java
+++ Stack.java
@@ -5,17 +5,25 @@ public class Stack {
 
 	public static void main(String[] args){
 		Stack myStack = new Stack();
-		myStack.insert(1);
-		myStack.insert(2);
-		myStack.insert(3);
+		myStack.push(1);
+		myStack.push(2);
+		myStack.push(3);
 
 		myStack.printAll();
+
+		System.out.println("Stack is empty? "+myStack.empty());
+
+		System.out.println(">>" + myStack.pop());
+		System.out.println(">>" + myStack.pop());
+		System.out.println(">>" + myStack.pop());
+
+		System.out.println("Stack is empty? "+myStack.empty());
 
 
 	}
 
 
-	public void insert (int storedNumber){
+	public void push (int storedNumber){
 		StackItem newItem = new StackItem(storedNumber);
 		if(firstItem == null){
 			this.firstItem = newItem;
@@ -32,13 +40,38 @@ public class Stack {
 		
 	}
 
-	public int retrieve (){
-		
-		return 0;
+	public int pop (){
+		if(firstItem == null){
+			System.out.println("Queue is currently empty");
+			return 0;
+		}
+		else
+		{
+			this.stackSize--;
+			int storedNumber = firstItem.storedNumber;
+
+			if(firstItem.nextItem != null){
+				firstItem.nextItem.previousItem = null;
+				firstItem = firstItem.nextItem;
+			}
+			else
+			{
+				firstItem = null;
+				lastItem = null;
+			}
+
+		return storedNumber;
+		}
 	}
 
-	public int empty(){
-		return 0;
+	public boolean empty(){
+		if(stackSize == 0){
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	public void printAll(){
