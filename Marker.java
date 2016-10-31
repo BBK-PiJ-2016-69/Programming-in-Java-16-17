230f0372d86d521321e381078dc787234f089419
Matthew Grint <mattgrint@gmail.com>
2016-10-31 14:37:09 +0000

    Completed marker class

--- Marker.java
+++ Marker.java
@@ -4,15 +4,46 @@ public class Marker{
 	int distinction = 0;
 	int pass = 0;
 	int fail = 0;
+	int invalid = 0;
+	int currentMark;
 
+	public static void main(String[] args){
+		Marker firstExam = new Marker();
+		firstExam.enterMarks();
+
+	}
 	
 	private void enterMarks(){
 
 		do{
 			System.out.println("Please enter a mark: ");
 			userInput = System.console().readLine();
-		}
-		while (userInput != "-1");
+			currentMark = Integer.parseInt(userInput);
 
+			if(currentMark == -1){
+				break;
+			}
+			else if (currentMark > 100){
+				invalid++;
+			}
+			else if (currentMark > 70){
+				students++;
+				distinction++;
+			}
+			else if (currentMark > 50){
+				students++;
+				pass++;
+			}
+			else if (currentMark > 0){
+				students++;
+				fail++;
+			}
+			else {
+				invalid++;
+			}
+		}
+		while (currentMark != -1);
+
+		System.out.println("There were " + students + " students with " + distinction + " distinctions, " + pass + " passes, " + fail + " fails and " + invalid + " invalid entries.");
 	}
 }
