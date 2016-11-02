e19390f550a2b8c660bf9092fe751d784feee9fe
Matthew Grint <mattgrint@gmail.com>
2016-11-02 14:33:00 +0000

    Added short hash functionality

---
+++ HashUtilities.java
@@ -0,0 +1,19 @@
+class HashUtilities{
+	
+
+	public static void main(String[] args){
+		String str = "This is a test string";
+		String str2 = "AAAAAAAAAAaaaa994803928409284029384&^*^*(&";
+
+		System.out.println("Hash Code: "+str.hashCode() + " and short hash: "+HashUtilities.shortHash(str.hashCode()));
+		System.out.println("Hash Code: "+str2.hashCode() + " and short hash: "+HashUtilities.shortHash(str2.hashCode()));
+	}
+		
+
+	static int shortHash(int toHash){
+		toHash = Math.abs(toHash);
+		return (toHash)%1000;
+	}
+
+
+}
